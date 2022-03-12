b06aeb4d3ea75e464c2c8519b8d8d782efabd62e
Juan Pablo <jpablo859@hotmail.com>
2022-03-12 05:43:59 -0300

    Navbar

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Navbar = () => {
+
+    return (
+        <div className="navbar navbar-dark bg-dark mb-4">
+            <span className="navbar-brand ps-4"><i className="fa fa-solid fa-notes-medical"></i> Saludtools</span>
+        </div>
+    )
+}
